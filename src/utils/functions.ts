004546a3d3014ac6eecd7b7c28035ba575c2e3b8
jmilkowski <jmilkowski@i2asolutions.com>
2023-05-11 17:09:06 +0200

    fix polish month names form

--- src/utils/functions.ts
+++ src/utils/functions.ts
@@ -1,15 +1,50 @@
+const replacePolishMonthNames = (date: string): string => {
+  const regex =
+    /(styczeń|luty|marzec|kwiecień|maj|czerwiec|lipiec|sierpień|wrzesień|październik|listopad|grudzień)/g;
+  return date.replaceAll(regex, (match) => {
+    switch (match) {
+      case "styczeń":
+        return "stycznia";
+      case "luty":
+        return "lutego";
+      case "marzec":
+        return "marca";
+      case "kwiecień":
+        return "kwietnia";
+      case "maj":
+        return "maja";
+      case "czerwiec":
+        return "czerwca";
+      case "lipiec":
+        return "lipca";
+      case "sierpień":
+        return "sierpnia";
+      case "wrzesień":
+        return "września";
+      case "październik":
+        return "października";
+      case "listopad":
+        return "listopada";
+      case "grudzień":
+        return "grudnia";
+      default:
+        return match;
+    }
+  });
+};
 export const getLocalizedMonthDateString = (
   locale: string,
   start_date_string: string,
   end_date_string: string | null = null
 ) => {
+  let date_string;
   const start_date = new Date(start_date_string);
 
   if (end_date_string) {
     const end_date = new Date(end_date_string);
-
+    date_string = "";
     if (start_date.getMonth() === end_date.getMonth()) {
-      return `${start_date.getDate()} - ${end_date.getDate()} ${start_date.toLocaleString(
+      date_string = `${start_date.getDate()} - ${end_date.getDate()} ${start_date.toLocaleString(
         locale,
         {
           month: "long",
@@ -17,19 +52,25 @@ export const getLocalizedMonthDateString = (
         }
       )}`;
     } else if (start_date.getFullYear() === end_date.getFullYear()) {
-      return `${start_date.getDate()} ${start_date.toLocaleString(locale, {
-        month: "long",
-        year: "numeric",
-      })} - ${end_date.getDate()} ${end_date.toLocaleString(locale, {
+      date_string = `${start_date.getDate()} ${start_date.toLocaleString(
+        locale,
+        {
+          month: "long",
+          year: "numeric",
+        }
+      )} - ${end_date.getDate()} ${end_date.toLocaleString(locale, {
         month: "long",
         year: "numeric",
       })} ${start_date.getFullYear()}`;
     }
+  } else {
+    date_string = `${start_date.getDate()} ${start_date.toLocaleString(locale, {
+      month: "long",
+      year: "numeric",
+    })}`;
   }
-  return `${start_date.getDate()} ${start_date.toLocaleString(locale, {
-    month: "long",
-    year: "numeric",
-  })}`;
+
+  return replacePolishMonthNames(date_string);
 };
 
 export const getLocalizedDayOfWeekString = (
@@ -40,5 +81,5 @@ export const getLocalizedDayOfWeekString = (
   const month = dateObj.toLocaleString(locale, { month: "long" });
   const weekday = dateObj.toLocaleString(locale, { weekday: "long" });
 
-  return `${dateObj.getDate()} ${month} (${weekday})`;
+  return replacePolishMonthNames(`${dateObj.getDate()} ${month} (${weekday})`);
 };
