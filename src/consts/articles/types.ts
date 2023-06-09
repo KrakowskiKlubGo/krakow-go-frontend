a7b2c5fda1be3a2cad1acc3faab6c549543f51a1
jmilkowski <jmilkowski@i2asolutions.com>
2023-06-09 17:05:20 +0200

    new env setup, basic articles feature

---
+++ src/consts/articles/types.ts
@@ -0,0 +1,13 @@
+export type ArticleListSchema = {
+  code: string;
+  menu_display_name: string;
+  language: string;
+};
+
+export type ArticleDetailSchema = {
+  code: string;
+  menu_display_name: string;
+  language: string;
+  html_content: string;
+  title: string;
+};
