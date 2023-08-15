4af49e317e2c0e2f367ea153cf8609aa0c5a0590
jmilkowski <jmilkowski@i2asolutions.com>
2023-08-15 16:17:13 +0200

    feature: tournament info tabs are # links

--- src/pages/turnieje/[code].tsx
+++ src/pages/turnieje/[code].tsx
@@ -55,8 +55,8 @@ export const getStaticProps: GetStaticProps = async (context) => {
 
 interface TabPanelProps {
   children?: React.ReactNode;
-  index: number;
-  value: number;
+  index: string;
+  value: string;
 }
 
 function TabPanel(props: TabPanelProps) {
@@ -75,7 +75,7 @@ function TabPanel(props: TabPanelProps) {
   );
 }
 
-function a11yProps(index: number) {
+function a11yProps(index: string) {
   return {
     id: `simple-tab-${index}`,
     "aria-controls": `simple-tabpanel-${index}`,
@@ -85,17 +85,28 @@ function a11yProps(index: number) {
 export default function TournamentDetail(
   data: InferGetStaticPropsType<typeof getStaticProps>
 ) {
-  const [value, setValue] = React.useState(0);
+  const [value, setValue] = React.useState("info");
+  React.useEffect(() => {
+    const default_value = window.location.hash.slice(1);
+    if (default_value !== "") {
+      setValue(default_value);
+    } else {
+      setValue("info");
+    }
+  }, []);
+
   const { lang } = useSelectedLanguage();
   const tournament = lang === "pl" ? data.tournament_pl : data.tournament_en;
   const registration_enable =
     tournament.registration_info.end_date != null
       ? new Date(tournament.registration_info.end_date) >= new Date()
       : true;
-  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+  const handleChange = (event: React.SyntheticEvent, newValue: string) => {
+    console.log(newValue);
     setValue(newValue);
   };
   const { t } = useTranslation();
+
   return (
     <>
       <Container>
@@ -129,39 +140,53 @@ export default function TournamentDetail(
               scrollButtons
               allowScrollButtonsMobile
             >
-              <Tab label={t("tournaments.info_tab")} {...a11yProps(0)} />
+              <Tab
+                label={t("tournaments.info_tab")}
+                href="#info"
+                {...a11yProps("info")}
+                value={"info"}
+              />
               <Tab
                 label={t("tournaments.registration_tab")}
-                {...a11yProps(1)}
+                {...a11yProps("register")}
                 disabled={!registration_enable}
+                href="#register"
+                value={"register"}
               />
               <Tab
                 label={t("tournaments.registered_players_tab")}
-                {...a11yProps(2)}
+                {...a11yProps("players")}
+                href="#players"
+                value={"players"}
               />
-              <Tab label={t("tournaments.results_tab")} {...a11yProps(2)} />
+              <Tab
+                label={t("tournaments.results_tab")}
+                {...a11yProps("results")}
+                href="#results"
+                value={"results"}
+              />
             </Tabs>
           </CenteredBox>
 
-          <TabPanel value={value} index={0}>
+          <TabPanel value={value} index={"info"}>
             <TournamentInfoPanel tournament_info={tournament.tournament_info} />
           </TabPanel>
           {registration_enable && (
-            <TabPanel value={value} index={1}>
+            <TabPanel value={value} index={"register"}>
               <RegistrationForm
                 registration_info={tournament.registration_info}
                 tournament_code={tournament.code}
               />
             </TabPanel>
           )}
-          <TabPanel value={value} index={2}>
+          <TabPanel value={value} index={"players"}>
             <RegisteredPlayersPanel
               tournament_code={tournament.code}
               registration_info={tournament.registration_info}
             />
           </TabPanel>
 
-          <TabPanel value={value} index={3}>
+          <TabPanel value={value} index={"results"}>
             <TournamentResultsPanel tournament_code={tournament.code} />
           </TabPanel>
         </Paper>
